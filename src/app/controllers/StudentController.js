111b46cca49166b2608c99d918fc180891f8143d
Gustavo Henrique de Lima Oliveira <gustavolima@Gustavos-MacBook-Pro.local>
2019-10-25 13:59:31 -0300

    GymPoint desafio 1 - completo

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,23 @@
+import * as Yup from 'yup';
+
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
@@ -18,8 +34,33 @@ class StudentController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    res.json({ ok: true });
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const student = await Student.findByPk(req.userId);
+
+    if (email && email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'student already exists.' });
+      }
+    }
+
+    const { id, name, age, weight, height } = await student.update(req.body);
+
+    return res.json({ id, name, email, age, weight, height });
   }
 }
 
